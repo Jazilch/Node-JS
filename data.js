4fee20e10313b9260cb6287000a2ffdf49cad5de
Jazilch <zilchjames5@gmail.com>
2017-10-08 12:46:38 -0400

    make it better

---
+++ data.js
@@ -0,0 +1,126 @@
+const data = [
+        {
+            "id": 1,
+            "firstName": "Darin",
+            "lastName": "Daignault",
+            "email": "ddaignault@hubspotpartners.com",
+            "country": "United States",
+            "availableDates": [
+                "2017-05-03",
+                "2017-05-06"
+            ]
+        },
+        {
+            "id": 2,
+            "firstName": "Crystal",
+            "lastName": "Brenna",
+            "email": "cbrenna@hubspotpartners.com",
+            "country": "Ireland",
+            "availableDates": [
+                "2017-04-27",
+                "2017-04-29",
+                "2017-04-30"
+            ]
+        },
+        {
+            "id": 3,
+            "firstName": "Janyce",
+            "lastName": "Gustison",
+            "email": "jgustison@hubspotpartners.com",
+            "country": "Spain",
+            "availableDates": [
+                "2017-04-29",
+                "2017-04-30",
+                "2017-05-01"
+            ]
+        },
+        {
+            "id": 4,
+            "firstName": "Tifany",
+            "lastName": "Mozie",
+            "email": "tmozie@hubspotpartners.com",
+            "country": "Spain",
+            "availableDates": [
+                "2017-04-28",
+                "2017-04-29",
+                "2017-05-01",
+                "2017-05-04"
+            ]
+        },
+        {
+            "id": 5,
+            "firstName": "Temple",
+            "lastName": "Affelt",
+            "email": "taffelt@hubspotpartners.com",
+            "country": "Spain",
+            "availableDates": [
+                "2017-04-28",
+                "2017-04-29",
+                "2017-05-02",
+                "2017-05-04"
+            ]
+        },
+        {
+            "id": 6,
+            "firstName": "Robyn",
+            "lastName": "Yarwood",
+            "email": "ryarwood@hubspotpartners.com",
+            "country": "Spain",
+            "availableDates": [
+                "2017-04-29",
+                "2017-04-30",
+                "2017-05-02",
+                "2017-05-03"
+            ]
+        },
+        {
+            "id": 7,
+            "firstName": "Shirlene",
+            "lastName": "Filipponi",
+            "email": "sfilipponi@hubspotpartners.com",
+            "country": "Spain",
+            "availableDates": [
+                "2017-04-30",
+                "2017-05-01"
+            ]
+        },
+        {
+            "id": 8,
+            "firstName": "Oliver",
+            "lastName": "Majica",
+            "email": "omajica@hubspotpartners.com",
+            "country": "Spain",
+            "availableDates": [
+                "2017-04-28",
+                "2017-04-29",
+                "2017-05-01",
+                "2017-05-03"
+            ]
+        },
+        {
+            "id": 9,
+            "firstName": "Wilber",
+            "lastName": "Zartman",
+            "email": "wzartman@hubspotpartners.com",
+            "country": "Spain",
+            "availableDates": [
+                "2017-04-29",
+                "2017-04-30",
+                "2017-05-02",
+                "2017-05-03"
+            ]
+        },
+        {
+            "id": 10,
+            "firstName": "Eugena",
+            "lastName": "Auther",
+            "email": "eauther@hubspotpartners.com",
+            "country": "United States",
+            "availableDates": [
+                "2017-05-04",
+                "2017-05-09"
+            ]
+        }
+]
+
+module.exports = data;
